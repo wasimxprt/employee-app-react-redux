5583569a9fc174600163706981106479766ff1c9
Wasim Sayyed <wasimxprt@gmail.com>
2020-09-29 19:26:40 +0530

    Updates

---
+++ precache-manifest.0da59f586c8f0d407e945d5fb2ea1993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c292f01cc6dfe7685291d1388fa5ae3",
+    "url": "/employee-app-react-redux/index.html"
+  },
+  {
+    "revision": "0e6ba0a656eacce98fd0",
+    "url": "/employee-app-react-redux/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "2e9051e71a8b5543d6e0",
+    "url": "/employee-app-react-redux/static/css/main.3ed1e716.chunk.css"
+  },
+  {
+    "revision": "0e6ba0a656eacce98fd0",
+    "url": "/employee-app-react-redux/static/js/2.289c931a.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/employee-app-react-redux/static/js/2.289c931a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9051e71a8b5543d6e0",
+    "url": "/employee-app-react-redux/static/js/main.167a0297.chunk.js"
+  },
+  {
+    "revision": "28c323273ab3d5e151f1",
+    "url": "/employee-app-react-redux/static/js/runtime-main.a588ab7b.js"
+  }
+]);
